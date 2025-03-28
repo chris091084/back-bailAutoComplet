2b6f43e09f75413321db6118f0503fc1f0be8a07
christian <christian.coley84@gmail.com>
2025-03-28 11:49:08 +0100

    fix

--- src/main/java/Back/bailAutoComplet/BailAutoComplet/Repository/AppartementRepository.java
+++ src/main/java/Back/bailAutoComplet/BailAutoComplet/Repository/AppartementRepository.java
@@ -3,5 +3,8 @@ package Back.bailAutoComplet.BailAutoComplet.Repository;
 import Back.bailAutoComplet.BailAutoComplet.model.Appartement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppartementRepository extends JpaRepository<Appartement,Long> {
+    List<Appartement> findAllByOrderByIdAsc();
 }
