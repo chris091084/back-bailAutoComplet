4760a7aaf1d7a017e4d82141078604f708922280
Christian <49199172+chris091084@users.noreply.github.com>
2025-06-07 16:18:53 +0200

    fix ok (#12)
    Co-authored-by: christian <christian.coley84@gmail.com>

--- src/main/java/Back/bailAutoComplet/BailAutoComplet/Service/AppartementService.java
+++ src/main/java/Back/bailAutoComplet/BailAutoComplet/Service/AppartementService.java
@@ -11,7 +11,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
