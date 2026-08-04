d262000d7c6b790f3a823dab5ddeb6f125ce83af
Christian COLEY <christian.coley@smile.com>
2026-08-04 08:42:49 +0200

    feat:  resiliation

--- src/locataire/locataire.entity.ts
+++ src/locataire/locataire.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { Appartement } from '../appartement/appartement.entity';
 import { numericTransformer } from '../common/numeric.transformer';
+import { ResultForm } from '../generation/result-form.entity';
 
 @Entity('locataire')
 export class Locataire {
@@ -30,4 +31,24 @@ export class Locataire {
   @ManyToOne(() => Appartement, { nullable: false })
   @JoinColumn({ name: 'appartement_id' })
   appartement!: Appartement;
+
+  /**
+   * Le result_form dont ce locataire est issu : il porte notamment `date_from`,
+   * la date de signature du bail reprise dans la lettre de congé. Nullable, les
+   * locataires saisis avant l'ajout de la colonne n'en ont pas.
+   */
+  @ManyToOne(() => ResultForm, { nullable: true })
+  @JoinColumn({ name: 'result_form_id' })
+  resultForm!: ResultForm | null;
+
+  /**
+   * Date d'envoi de la lettre de congé, `null` tant qu'aucune n'est partie.
+   * Renseignée par le front une fois le mail accepté par /mail/send.
+   */
+  @Column({
+    name: 'resiliation_envoyee_le',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  resiliationEnvoyeeLe!: Date | null;
 }
--- src/locataire/locataire.module.ts
+++ src/locataire/locataire.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Appartement } from '../appartement/appartement.entity';
+import { ResultForm } from '../generation/result-form.entity';
 import { LocataireController } from './locataire.controller';
 import { Locataire } from './locataire.entity';
 import { LocataireService } from './locataire.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Locataire, Appartement])],
+  imports: [TypeOrmModule.forFeature([Locataire, Appartement, ResultForm])],
   controllers: [LocataireController],
   providers: [LocataireService],
 })
